60ae00db7aaef6585ab8688fab5dfbde5ae2edc5
IamAKX <akx.sonu@gmail.com>
2017-02-25 02:30:13 +0530

    init

---
+++ src/AkashApplications/networkmanager/ServerConstants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AkashApplications.networkmanager;
+
+/**
+ *
+ * @author akash
+ */
+public class ServerConstants {
+
+    public static final String BASE_URL = "http://plywoodmanor.16mb.com/";
+    public static final String ADD_PRODUCT = BASE_URL + "AddStock.php";
+}
